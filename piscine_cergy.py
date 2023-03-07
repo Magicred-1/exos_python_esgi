9688c9cfe28bd1c74300df7f6b04d0dc5b8f8dbd
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2023-03-07 12:55:02 +0100

    Deuxième Breakpoint

--- piscine_cergy.py
+++ piscine_cergy.py
@@ -1,6 +1,7 @@
 from math import sqrt
 
 # Premier Breakpoint
+# Exercice 1
 def calculVitesse(temps: int, distance: int):
     res = (distance / temps)
 
@@ -8,6 +9,7 @@ def calculVitesse(temps: int, distance: int):
 
 # print(calculVitesse(2, 100))
 
+# Exercice 2
 def saisirNomEtAge():
     nom: str = input("Quel est votre nom ? ")
     age: int = int(input("Quel est votre âge ? "))
@@ -15,6 +17,7 @@ def saisirNomEtAge():
 
 # saisirNomEtAge()
 
+# Exercice 3
 def flottant(nombre: float):
     if nombre > 0 or nombre == 0:
         print(sqrt(nombre))
@@ -24,6 +27,7 @@ def flottant(nombre: float):
 # flottant(4)
 # flottant(-2)
 
+# Exercice 4
 def lexicographique(mot1: str, mot2: str):
     if mot1 < mot2:
         print(mot1)
@@ -35,6 +39,7 @@ def lexicographique(mot1: str, mot2: str):
 # lexicographique("bonjour", "bonsoir")
 # lexicographique("bonjour", "bonjour")
 
+# Exercice 5
 def enceintePressurisee():
     pSeuil = 2.3
     vSeuil = 7.41
@@ -53,3 +58,71 @@ def enceintePressurisee():
             print("tout va bien")
 
 # enceintePressurisee()
+
+# Deuxième Breakpoint
+
+# Exercice 6
+def verifierSaisie():
+    arobase = "@"
+    com = ".com"
+    saisie: str = input("Saisissez votre adresse mail : ")
+
+    if arobase in saisie and com in saisie:
+        print("Adresse mail valide")
+
+# verifierSaisie()
+
+# Exercice 7
+def afficherDixMessage(message: str):
+    for i in range(10):
+        print(message)
+
+# afficherDixMessage("Bonjour")
+
+# Exercice 8
+def afficherMessageLettreParLettre(message: str):
+    for i in message:
+        print(i)
+
+# afficherMessageLettreParLettre("Bonjour je suis un message")
+
+# Exercice 9
+def increment(a: int = 0, b: int = 10):
+    while a < b:
+        for i in range(a, b):
+            print(i)
+
+# increment()
+
+# Exercice 10
+def desincrement(b: int = 10):
+    while b > 0:
+        for i in range(b):
+            b -= 1
+            if b % 2 == 1:
+                print(b)
+
+# desincrement()
+
+# Exercice 11
+def saisieFiltreEntier(entier: int):
+    if entier >= 1 and entier <= 10:
+        print(entier)
+
+# saisieFiltreEntier(5)
+# saisieFiltreEntier(11)
+
+# Exercice 12 & 13
+def afficherCaracteresChaine(chaine: str, liste: list):
+    for i in chaine:
+        print(i)
+
+    for j in range(0, len(liste), 3):
+        print(liste[j])
+
+    for k in range(0, len(liste), 2):
+        if liste[k] % 2 == 0:
+            print(liste[k])
+
+afficherCaracteresChaine("Bonjour", [34, 56, 78, 90, 245, 456, 3553])
+
